cd4b4d0e6fbb4972d3ee5b50cf884c023af453a7
matteopost <matteopostinghel@icloud.com>
2024-04-19 17:02:37 +0200

    ho reso gli articoli cliccabili non solo al titolo ma a tutta la card

--- theme/template-parts/content/content-excerpt.php
+++ theme/template-parts/content/content-excerpt.php
@@ -9,31 +9,30 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="group">
 
-	<div class="pb-2 img-container">
-		<?php the_post_thumbnail('medium'); ?>
-	</div>
 
-	<header class="">
+<article id="post-<?php the_ID(); ?>" <?php post_class('group'); ?>>
+    <a href="<?php the_permalink(); ?>" class="block">
+        <div class="pb-2 img-container">
+            <?php the_post_thumbnail('medium'); ?>
+        </div>
 
-		<p class="text-black text-xs uppercase"><?php single_term_title(); ?> / <?php the_field('tema') ?></p>
-
-		<?php
-		if ( is_sticky() && is_home() && ! is_paged() ) {
-			printf( '%s', esc_html_x( 'Featured', 'post', 'gravity' ) );
-		}
-		the_title( sprintf( '<h2 class="text-black text-xl"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		?>
-	</header><!-- .entry-header -->
-
-<!-- 
-	<div <?php gravity_content_class( 'entry-content' ); ?>>
+        <header class="">
+            <p class="text-black text-xs uppercase"><?php single_term_title(); ?> / <?php the_field('tema') ?></p>
+            <?php
+            if ( is_sticky() && is_home() && ! is_paged() ) {
+                printf( '%s', esc_html_x( 'Featured', 'post', 'gravity' ) );
+            }
+            the_title( '<h2 class="text-black text-xl">', '</h2>' );
+            ?>
+        </header><!-- .entry-header -->
+		<!-- 
+		<div <?php gravity_content_class( 'entry-content' ); ?>>
 		<?php the_excerpt(); ?>
-	</div>
+		</div>
 
-	<footer class="entry-footer">
-		<?php gravity_entry_footer(); ?>
-	</footer> -->
-
-</article><!-- #post-${ID} -->
+		<footer class="entry-footer">
+			<?php gravity_entry_footer(); ?>
+		</footer> -->
+    </a>
+</article>
