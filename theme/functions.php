847191270260041484f8b1424553b018be604184
matteopost <matteopostinghel@icloud.com>
2024-04-22 15:09:09 +0200

    creato file blocks.json inserito function in funciton.php

--- theme/functions.php
+++ theme/functions.php
@@ -208,3 +208,24 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
+
+
+/**
+ * We use WordPress's init hook to make sure
+ * our blocks are registered early in the loading
+ * process.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/init/
+ */
+
+function gravity_register_acf_blocks() {
+    /**
+     * We register our block's with WordPress's handy
+     * register_block_type();
+     *
+     * @link https://developer.wordpress.org/reference/functions/register_block_type/
+     */
+    register_block_type( __DIR__ . '/blocks/testimonial' );
+}
+// Here we call our tt3child_register_acf_block() function on init.
+add_action( 'init', 'gravity_register_acf_blocks' );
