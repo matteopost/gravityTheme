c438bb8916e544d6c8bc6d78a006daaac566293e
matteopost <matteopostinghel@icloud.com>
2024-04-22 17:23:07 +0200

    provato di fare funzionare alpine.js

--- theme/blocks/testimonial/testimonial.php
+++ theme/blocks/testimonial/testimonial.php
@@ -14,27 +14,30 @@ $email              = get_field( 'email' );
 
 ?>
 
+<div x-data="{open : false}" class="mb-5">
+    <div @click="open = !open">
+        <?php if ( !empty( $nome_cognome ) ) : ?>
+            <h3 class="pb-4">
+                <?php echo esc_html( $nome_cognome ); ?>
+            </h3>
+        <?php endif; ?>
 
-<div class="">
-    <?php if ( !empty( $nome_cognome ) ) : ?>
-        <h3 class="pb-4">
-            <?php echo esc_html( $nome_cognome ); ?>
-        </h3>
-    <?php endif; ?>
+        <?php if ( $image ) : ?>
+            <div class="">
+                <?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'testimonial__img' ) ); ?>
+            </div>
+        <?php endif; ?>
 
-    <?php if ( $image ) : ?>
-        <div class="">
-            <?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'testimonial__img' ) ); ?>
-        </div>
-    <?php endif; ?>
+        <?php if ( !empty( $posizione ) ) : ?>
+            <p class="text-sm uppercase pt-2">
+                <?php echo wp_kses_post( $posizione ); ?>
+            </p>
+        <?php endif; ?>
+    </div>
 
-    <?php if ( !empty( $posizione ) ) : ?>
-        <p class="text-sm uppercase pt-2">
-            <?php echo wp_kses_post( $posizione ); ?>
-        </p>
-    <?php endif; ?>
+    <button @click="open = !open" >More</button>
 
-    <div class="">
+    <div x-show="open">
         <?php if ( !empty( $biografia ) ) : ?>
             <p class="text-sm pt-2">
                 <?php echo wp_kses_post( $biografia ); ?>
