47b04d78955c2491a02dea5f144a51e2e9742d6a
matteopost <matteopostinghel@icloud.com>
2024-04-23 10:43:20 +0200

    creato i filtri con alpine

--- theme/archive.php
+++ theme/archive.php
@@ -19,7 +19,41 @@ get_header();
 				<?php single_term_title( '<h1 class="page-title uppercase">', '</h1>' ); ?> 
 			</header><!-- .page-header -->
 
+			<!-- Filtri -->
+			<!-- <?php $alpine_data = (!$is_preview) ? 'x-data="{open: false}"' : ''; ?> -->
 
+			<div <?= $alpine_data; ?> class="border-b border-black p-5">
+
+
+				<div class="flex justify-between">
+					<button @click="open = !open" class="">Filtri</button>
+					<button @click="open = !open" class="">+</button>
+				</div>
+
+
+				<div x-show="open" class="grid grid-cols-6 py-5 gap-5">
+					<div class="space-y-2">
+						<p class="text-base uppercase">Tema</p>
+						<button class="block">Suolo</button>
+						<button class="block">SLOW</button>
+						<button class="block">Spazi</button>
+					</div>
+
+					<div class="space-y-2">
+						<p class="text-base uppercase">Tipologia</p>
+						<button class="block">Progetto</button>
+						<button class="block">Ricerca</button>
+					</div>
+
+					<div class="space-y-2">
+						<p class="text-base uppercase">Stato</p>
+						<button class="block">In corso</button>
+						<button class="block">Concluso</button>
+					</div>
+				</div>
+			</div>
+
+			<!-- Post -->
 			<div class="grid grid-cols-1 md:grid-cols-2 gap-5 p-5">
 				<?php
 				// Start the Loop.
